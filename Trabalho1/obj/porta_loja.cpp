15574cf3c50ead7ceab1e58bb80a8117c4220646
Michel Melo <michelmelosj25@hotmail.com>
2019-04-25 12:00:13 -0300

    criacao de arvore e loja

---
+++ Trabalho1/obj/porta_loja.cpp
@@ -0,0 +1,6 @@
+#include "porta_loja.h"
+
+Porta_Loja::Porta_Loja()
+{
+
+}
---
+++ Trabalho1/obj/porta_loja.h
@@ -0,0 +1,11 @@
+#ifndef PORTA_LOJA_H
+#define PORTA_LOJA_H
+
+
+class Porta_Loja
+{
+public:
+    Porta_Loja();
+};
+
+#endif // PORTA_LOJA_H
